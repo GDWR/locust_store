cd10a86f9cfd3299c93eb8c66ad55e197b6076b6
GDWR <gregory.dwr@gmail.com>
2024-10-31 11:19:11 +0000

    Add devcontainer

--- README.md
+++ README.md
@@ -7,12 +7,3 @@ pdm run dev # Start api, runs on localhost:8000 by default
 
 pdm run test # Start locust, do magic
 ```
-
-## Test with postgresql
-
-start postgres, you can use docker compose for ease.
-```bash
-docker compose up
-```
-
-and change locust_store/database.py to uncomment the postgresql connection and comment sqlite3.
--- locust_store/database.py
+++ locust_store/database.py
@@ -3,7 +3,7 @@ from sqlmodel import SQLModel, create_engine
 # Ensure models are loaded when creating metadata
 from .models import *
 
-# engine = create_engine("postgresql://postgres:password@localhost:5432")
+# engine = create_engine("postgresql://postgres:postgres@localhost:5432")
 engine = create_engine("sqlite:///database.db")
 
 SQLModel.metadata.create_all(engine)
